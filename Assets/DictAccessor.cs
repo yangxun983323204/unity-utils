1a544641e97ee712f852380fc51b1ae9f84481ae
杨循 <929500@nd.com>
2020-09-23 17:12:57 +0800

    update

---
+++ Assets/DictAccessor.cs
@@ -0,0 +1,123 @@
+﻿/*************************************************************************
+ *  Copyright © 2019 NetDragon Websoft Inc. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  DictAccessor.cs
+ *  Description  :  
+ *------------------------------------------------------------------------
+ *  Author       :  yangxun
+ *  Version      :  v0.1
+ *  Date         :  2020/6/9 星期二 下午 2:22:13
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace YX
+{
+    /// <summary>
+    /// 字典的lazy存取封装
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    class DictAccessor<TKey,TValue>
+    {
+        public IDictionary<TKey, TValue> Dict { get; set; }
+        /// <summary>
+        /// 获取字典中key的值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public TValue Get(TKey key,TValue def = default(TValue))
+        {
+            if (Dict == null)
+                return def;
+
+            Dict.TryGetValue(key, out def);
+            return def;
+        }
+        /// <summary>
+        /// 设置字典的键和值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val"></param>
+        public void Set(TKey key,TValue val)
+        {
+            if (Dict == null)
+                Dict = new Dictionary<TKey, TValue>(4);
+
+            if (Dict.ContainsKey(key))
+                Dict[key] = val;
+            else
+                Dict.Add(key, val);
+        }
+
+        /// <summary>
+        /// 移除key
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(TKey key)
+        {
+            if (Dict == null)
+                return;
+            else
+            {
+                if(Dict.ContainsKey(key))
+                    Dict.Remove(key);
+            }
+        }
+    }
+
+    class DictAccessor
+    {
+        public System.Collections.IDictionary Dict { get; set; }
+        /// <summary>
+        /// 获取字典中key的值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public object Get(object key, object def = default(object))
+        {
+            if (Dict == null)
+                return def;
+            if (Dict.Contains(key))
+                def = Dict[key];
+            return def;
+        }
+        /// <summary>
+        /// 设置字典的键和值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val"></param>
+        public void Set(object key, object val)
+        {
+            if (Dict == null)
+                Dict = new Dictionary<object,object>(4);
+
+            if (Dict.Contains(key))
+                Dict[key] = val;
+            else
+                Dict.Add(key, val);
+        }
+
+        /// <summary>
+        /// 移除key
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(object key)
+        {
+            if (Dict == null)
+                return;
+            else
+            {
+                if (Dict.Contains(key))
+                    Dict.Remove(key);
+            }
+        }
+    }
+}
