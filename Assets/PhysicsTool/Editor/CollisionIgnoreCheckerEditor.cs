cf21a057f7c2dace0a8864a1f10484f60933f74a
杨循 <929500@nd.com>
2020-11-15 17:54:44 +0800

    添加两个物体之间是否可以碰撞的检测工具

---
+++ Assets/PhysicsTool/Editor/CollisionIgnoreCheckerEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CollisionIgnoreChecker))]
+public class CollisionIgnoreCheckerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("检查"))
+        {
+            (target as CollisionIgnoreChecker).CheckIsIgnore();
+        }
+    }
+}
