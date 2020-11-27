c8206eaab9586d2f5ded4569ab4182fdeb535f3a
杨循 <929500@nd.com>
2020-11-27 15:26:47 +0800

    字符串颜色

---
+++ Assets/StrEx.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YX
+{
+    public static class StrEx
+    {
+        public static string Dye(this string self, Color col)
+        {
+            var val = ColorUtility.ToHtmlStringRGBA(col);
+            return "<color=#" + val + ">" + self + "</color>";
+        }
+
+        public static string Dye(this string self, float r,float g,float b,float a)
+        {
+            return Dye(self, new Color(r, g, b, a));
+        }
+    }
+}
---
+++ Assets/Test/TestStrEx.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YX;
+
+public class TestStrEx : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("啊哒~~~~".Dye(Color.red));
+        Debug.Log("啊哒~~~~".Dye(0,0.5f,0.5f,1));
+    }
+}
