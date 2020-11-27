d5dde50217d7ef7728a7b496033ebe468c68f9de
杨循 <929500@nd.com>
2020-11-27 12:04:54 +0800

    增加文件工具

---
+++ Assets/FileUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace YX
+{
+    public class FileUtils
+    {
+        public static void WriteAllBytes(string path,byte[] bytes)
+        {
+            bool nofile = false;
+            var dir = Path.GetDirectoryName(path);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+                nofile = true;
+            }
+
+            if (!nofile && File.Exists(path))
+                File.Delete(path);
+
+            File.WriteAllBytes(path, bytes);
+        }
+    }
+}
