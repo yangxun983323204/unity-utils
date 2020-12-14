54c6bd4da2985d4cf18f028f4438236106da19a0
杨循 <929500@nd.com>
2020-12-14 17:52:09 +0800

    计数

---
+++ Assets/Count.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YX
+{
+    public class Count<TKey>
+    {
+        public event Action<TKey> onNew, onDelete;
+
+        private Dictionary<TKey, int> _dict;
+
+        public Count(int capacity)
+        {
+            _dict = new Dictionary<TKey, int>(capacity);
+        }
+
+        public int Add(TKey key)
+        {
+            int val = 0;
+            _dict.TryGetValue(key, out val);
+            if (val <= 0)
+                onNew?.Invoke(key);
+
+            val++;
+            _dict[key] = val;
+            return val;
+        }
+
+        public int Release(TKey key)
+        {
+            int val = 0;
+            if (_dict.TryGetValue(key,out val))
+            {
+                if (val > 0)
+                    onDelete?.Invoke(key);
+
+                val--;
+                if (val < 0)
+                    val = 0;
+
+                _dict[key] = val;
+            }
+
+            return val;
+        }
+
+        public int GetCount(TKey key)
+        {
+            int val = 0;
+            _dict.TryGetValue(key, out val);
+            return val;
+        }
+    }
+}
