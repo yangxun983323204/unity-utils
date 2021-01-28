cd9004cfc151df58890c08b2e833b5b575303e77
杨循 <929500@nd.com>
2021-01-28 10:51:56 +0800

    增加Line类

--- Assets/Interpolation.cs
+++ Assets/Interpolation.cs
@@ -9,18 +9,18 @@ namespace YX
         /// <summary>
         /// 凹曲线插值
         /// </summary>
-        public static float Concave(float from, float to, float t)
+        public static float Concave(float from, float to, float t,float intensity = 2)
         {
-            float t1 = Mathf.Pow(t, 2);
+            float t1 = Mathf.Pow(t, intensity);
             return Mathf.Lerp(from, to, t1);
         }
 
         /// <summary>
         /// 凸曲线插值
         /// </summary>
-        public static float Raise(float from, float to, float t)
+        public static float Raise(float from, float to, float t, float intensity = 2)
         {
-            float t1 = 1 - Mathf.Pow(t, 2);
+            float t1 = 1 - Mathf.Pow(t, intensity);
             return Mathf.Lerp(from, to, t1);
         }
     }
---
+++ Assets/Line.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YX
+{
+    public struct Line
+    {
+        private Vector3 _o;
+        private Vector3 _dir;
+
+        public Line(Vector3 point,Vector3 dir)
+        {
+            _o = point;
+            _dir = dir.normalized;
+        }
+
+        public bool InLine(Vector3 pos)
+        {
+            var op = pos - _o;
+            var proj = Vector3.Dot(op, _dir);
+            return proj == 0;
+        }
+
+        public float Distance(Vector3 pos)
+        {
+            var op = pos - _o;
+            var proj = Vector3.Dot(op, _dir);
+            if (proj == 0)
+                return 0;
+
+            return (op - _dir * proj).magnitude;
+        }
+    }
+
+}
