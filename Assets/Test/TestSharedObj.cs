98caa2cf8b1cdc0eaf40a3cdd345c1edf24ef725
杨循 <929500@nd.com>
2021-02-24 18:43:08 +0800

    增加一点工具

---
+++ Assets/Test/TestSharedObj.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YX;
+
+public class TestSharedObj : MonoBehaviour
+{
+    public class TA
+    {
+        public int a;
+        public string b;
+
+        public override string ToString()
+        {
+            return $"a:{a},b:{b}";
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var raw = new TA() { a= 2,b="abc" };
+        using (var p1 = new SharedObj<TA>()) {
+            Debug.Assert(p1.Count == 0);
+            p1.Set(raw, n => { Debug.Log("删除！"); });
+            Debug.Assert(p1.Count == 1);
+
+            using (var p2 = p1.Share()) {
+                Debug.Assert(p1.Count == 2);
+                Debug.Assert(p2.Count == 2);
+                p2.Tar.a = 3;
+                p2.Tar.b = "456";
+                Debug.Assert(p1.Tar.a == 3);
+                Debug.Assert(p1.Tar.b == "456");
+            }
+
+            Debug.Assert(p1.Count == 1);
+        }
+    }
+}
---
+++ Assets/UI/WrapLayoutGroup.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace YX
+{
+    [AddComponentMenu("Layout/Wrap Layout Group", 153)]
+    public class WrapLayoutGroup : LayoutGroup
+    {
+        private Vector2[] childPos = new Vector2[0];
+        private Bounds _childrenBounds;
+
+        protected WrapLayoutGroup()
+        { }
+
+        public override void CalculateLayoutInputVertical()
+        {
+            SetLayoutInputForAxis(_childrenBounds.size.y, _childrenBounds.size.y, -1, 1);
+        }
+
+        public override void SetLayoutHorizontal()
+        {
+            CalcRequiredSize();
+            SetLayoutInputForAxis(_childrenBounds.size.x, _childrenBounds.size.x, -1, 0);
+        }
+
+        public override void SetLayoutVertical()
+        {
+            SetChildren();
+        }
+
+        private void SetChildren()
+        {
+            Vector3 offset = _childrenBounds.center;
+            for (int i = 0; i < rectChildren.Count; i++)
+            {
+                var c = rectChildren[i];
+                c.transform.localPosition -= offset;
+            }
+        }
+
+        private void CalcRequiredSize()
+        {
+            _childrenBounds = default(Bounds);
+            for (int i = 0; i < rectChildren.Count; i++)
+            {
+                var c = rectChildren[i];
+                if (i == 0)
+                    _childrenBounds = RectTransformUtility.CalculateRelativeRectTransformBounds(rectTransform, c);
+                else
+                    _childrenBounds.Encapsulate(RectTransformUtility.CalculateRelativeRectTransformBounds(rectTransform, c));
+            }
+        }
+    }
+}
