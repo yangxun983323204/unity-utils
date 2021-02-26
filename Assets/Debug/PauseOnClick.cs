8b3f0b7d9c207344cfdf03482dcc1bd5bc935671
杨循 <929500@nd.com>
2021-02-26 17:01:49 +0800

    增加编辑器暂停工具

---
+++ Assets/Debug/PauseOnClick.cs
@@ -0,0 +1,37 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEditor;
+
+namespace YX
+{
+    public class PauseOnClick : MonoBehaviour, IPointerClickHandler
+    {
+        public int DelayFrame = 0;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (DelayFrame <= 0)
+            {
+                EditorApplication.isPaused = true;
+            }
+            else
+            {
+                StartCoroutine(DelayPause(DelayFrame));
+            }
+        }
+
+        private IEnumerator DelayPause(int frame)
+        {
+            var f = new WaitForEndOfFrame();
+            for (int i = 0; i < frame; i++)
+            {
+                yield return f;
+                EditorApplication.isPaused = true;
+            }
+        }
+    }
+}
+#endif
---
+++ Assets/Debug/PauseOnLog.cs
@@ -0,0 +1,65 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace YX
+{
+    /// <summary>
+    /// 在log输出包含指定关键字的时候暂停编辑器，方便单步执行
+    /// </summary>
+    public class PauseOnLog : MonoBehaviour
+    {
+        public enum KeyWordsType
+        {
+            Start,
+            Contain,
+            End,
+        }
+
+        public KeyWordsType Type = KeyWordsType.Start;
+        public string KeyWords;
+
+        private void Start()
+        {
+            Application.logMessageReceived += OnRecLog;
+        }
+
+        private void OnDestroy()
+        {
+            Application.logMessageReceived -= OnRecLog;
+        }
+
+        private void OnRecLog(string condition, string stackTrace, LogType type)
+        {
+            if (string.IsNullOrEmpty(KeyWords))
+                return;
+
+            switch (Type)
+            {
+                case KeyWordsType.Start:
+                    if (condition.StartsWith(KeyWords))
+                    {
+                        EditorApplication.isPaused = true;
+                    }
+                    break;
+                case KeyWordsType.Contain:
+                    if (condition.Contains(KeyWords))
+                    {
+                        EditorApplication.isPaused = true;
+                    }
+                    break;
+                case KeyWordsType.End:
+                    if (condition.EndsWith(KeyWords))
+                    {
+                        EditorApplication.isPaused = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+#endif
