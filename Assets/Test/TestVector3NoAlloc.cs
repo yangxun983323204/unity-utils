71812f5526189d88d876dbf6f0d0e87dd1ed6713
杨循 <929500@nd.com>
2021-03-04 18:18:20 +0800

    为Vector3提供不创建新对象的操作

---
+++ Assets/Test/TestVector3NoAlloc.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YX;
+
+public class TestVector3NoAlloc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var t1 = new Vector3(1, -5, 3);
+        var t2 = new Vector3(-5, -4, 6);
+
+        var t1n = t1;
+        Vector3NoAlloc.Add(ref t1n, ref t2);
+        Debug.Assert(t1 + t2 == t1n);
+
+        t1n = t1;
+        Vector3NoAlloc.Sub(ref t1n, ref t2);
+        Debug.Assert(t1 - t2 == t1n);
+
+        t1n = t1;
+        Vector3NoAlloc.Scale(ref t1n, 3);
+        Debug.Assert(t1 * 3 == t1n);
+
+        t1n = t1;
+        Vector3NoAlloc.Cross(ref t1n, ref t2);
+        var c = Vector3.Cross(t1, t2);
+        Debug.Assert(c == t1n,$"{c}<--->{t1n}");
+    }
+}
---
+++ Assets/Vector3NoAlloc.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YX
+{
+    public static class Vector3NoAlloc
+    {
+        public static void Add(ref Vector3 self,ref Vector3 other)
+        {
+            self.x += other.x;
+            self.y += other.y;
+            self.z += other.z;
+        }
+
+        public static void Sub(ref Vector3 self, ref Vector3 other)
+        {
+            self.x -= other.x;
+            self.y -= other.y;
+            self.z -= other.z;
+        }
+
+        public static void Scale(ref Vector3 self, float s)
+        {
+            self.x *= s;
+            self.y *= s;
+            self.z *= s;
+        }
+
+        public static void Cross(ref Vector3 self, ref Vector3 other)
+        {
+            var x = self.y * other.z - self.z * other.y;
+            var y = self.z * other.x - self.x * other.z;
+            var z = self.x * other.y - self.y * other.x;
+            self.x = x;
+            self.y = y;
+            self.z = z;
+        }
+    }
+}
