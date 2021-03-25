96ee54fa2ea37de7d09def9096accf667fb27013
杨循 <929500@nd.com>
2021-03-25 16:59:26 +0800

    新增编辑器工具UGUIInvoker，用于在无源码时触发UI的功能。

---
+++ Assets/Debug/Editor/UGUIInvoker.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Reflection;
+
+namespace YX
+{
+    public class UGUIInvoker : EditorWindow
+    {
+        private Button _btn;
+        private Toggle _tog;
+        private InputField _input;
+        private MonoBehaviour _mono;
+        private string _monoFunc;
+
+        [MenuItem("Debug/UGUI Invoker")]
+        public static void Open()
+        {
+            var wnd = GetWindow<UGUIInvoker>("UGUI Invoker");
+            wnd.Show();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            EditorGUILayout.Space();
+            Btn();
+            Tog();
+            Input();
+            Mono();
+            EditorGUILayout.Space();
+            EditorGUILayout.EndVertical();
+        }
+
+        private void Btn()
+        {
+            EditorGUILayout.BeginHorizontal();
+            _btn = EditorGUILayout.ObjectField(_btn, typeof(Button), true) as Button;
+            if (GUILayout.Button("Click") && _btn!=null)
+            {
+                _btn.onClick.Invoke();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void Tog()
+        {
+            EditorGUILayout.BeginHorizontal();
+            _tog = EditorGUILayout.ObjectField(_tog, typeof(Toggle), true) as Toggle;
+            if (GUILayout.Button("Tog") && _tog != null)
+            {
+                _tog.isOn = !_tog.isOn;
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void Input()
+        {
+            EditorGUILayout.BeginHorizontal();
+            _input = EditorGUILayout.ObjectField(_input, typeof(InputField), true) as InputField;
+            if (GUILayout.Button("Submit") && _input != null)
+            {
+                _input.OnSubmit(null);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void Mono()
+        {
+            EditorGUILayout.BeginHorizontal();
+            _mono = EditorGUILayout.ObjectField(_mono, typeof(MonoBehaviour), true) as MonoBehaviour;
+            _monoFunc = EditorGUILayout.TextField(_monoFunc);
+            if (GUILayout.Button("Call") && _mono != null && _monoFunc!=null)
+            {
+                var t = _mono.GetType();
+                var m = t.GetMethod(_monoFunc, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                m.Invoke(_mono, new object[] { });
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/Test/TestUGUICall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestUGUICall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var btn = GetComponent<Button>();
+        if (btn)
+        {
+            btn.onClick.AddListener(() => { Debug.Log("click"); });
+        }
+    }
+
+    private void Test()
+    {
+        Debug.Log("Test~~~");
+    }
+}
