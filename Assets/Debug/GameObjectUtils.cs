f3a13f18da65413b00ff8fa548d508f8ceaecaa2
杨循 <929500@nd.com>
2021-07-12 15:56:12 +0800

    增加GameObjectUtils

---
+++ Assets/Debug/GameObjectUtils.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace YX
+{
+    public static class GameObjectUtils
+    {
+        static StringBuilder _sb = new StringBuilder(256);
+        static Stack<string> _stack = new Stack<string>(20);
+
+        public static string GetFullPath(Transform tran)
+        {
+            if (tran == null)
+                return null;
+
+            _sb.Clear();
+            var p = tran;
+            while (p != null)
+            {
+                _stack.Push(p.gameObject.name);
+                p = p.parent;
+            }
+
+            while (_stack.Count > 0)
+            {
+                var node = _stack.Pop();
+                _sb.Append(node);
+                _sb.Append("/");
+            }
+            _sb.Remove(_sb.Length - 1, 1);
+
+            return _sb.ToString();
+        }
+    }
+}
---
+++ Assets/Test/TestGameObjectUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YX;
+
+public class TestGameObjectUtils : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestGetFullPath();
+    }
+
+    void TestGetFullPath()
+    {
+        var g0 = new GameObject("一");
+        var g1 = new GameObject("二"); g1.transform.SetParent(g0.transform);
+        var g2 = new GameObject("三"); g2.transform.SetParent(g1.transform);
+        var g3 = new GameObject("四"); g3.transform.SetParent(g2.transform);
+        var g4 = new GameObject("五"); g4.transform.SetParent(g3.transform);
+
+        Debug.Log(GameObjectUtils.GetFullPath(null));
+        Debug.Log(GameObjectUtils.GetFullPath(g0.transform));
+        Debug.Log(GameObjectUtils.GetFullPath(g1.transform));
+        Debug.Log(GameObjectUtils.GetFullPath(g2.transform));
+        Debug.Log(GameObjectUtils.GetFullPath(g3.transform));
+        Debug.Log(GameObjectUtils.GetFullPath(g4.transform));
+    }
+}
